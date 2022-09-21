882ee9d273e5864849a36064433a5754ddc741c1
Gloria <gloriamendezs13@gmail.com>
2022-09-21 09:05:49 +0200

    Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductFormComponent } from './product/product-form/product-form.component';
+import { ProductsComponent } from './product/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: ProductsComponent},
+  {path: 'home/form', component: ProductFormComponent},
+  {path: 'home/form/:id', component: ProductFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
